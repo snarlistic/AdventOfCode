d0d35608d439c0b608a61cb3393716434be8c72b
Freddy Spierenburg <freddy@snarl.nl>
2022-12-11 10:03:45 +0100

    Solution to day 10 part 2.

--- 2022/day.10/solution.py
+++ 2022/day.10/solution.py
@@ -13,12 +13,23 @@ def process_instruction(line, x):
 if __name__ == '__main__':
 	x = 1
 	cycles = []
+	crt = []
+	crt_index = 0
+	crt_width = 40
 	with open('input.txt') as file:
 		for line in file.readlines():
-			( x, burned_cycles) = process_instruction(line, x)
+			( x_new, burned_cycles) = process_instruction(line, x)
 			cycles +=  burned_cycles
+			for burned_cycle in burned_cycles:
+				crt += [ '#' if crt_index % crt_width in range(x - 1, x + 2) else '.' ]
+				crt_index += 1
+			x = x_new
 	cycles += [ x ]
 	strength = 0
 	for cycle in [ 20, 60, 100, 140, 180, 220 ]:
 		strength += cycle * cycles[cycle - 1]
 	print('Answer 1: {}'.format(strength))
+	print('Answer 2:')
+	rows = range(len(crt))[0::crt_width]
+	for row in rows:
+		print('  {}'.format(''.join(crt[row:row + crt_width])))
